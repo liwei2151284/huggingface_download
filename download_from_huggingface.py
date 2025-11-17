57050d15e31469c8aae6a7cb86fc2cd72fb61829
liwei2151284 <445169331@qq.com>
2025-11-17 14:21:06 +0800

    Update download_from_huggingface.py

--- download_from_huggingface.py
+++ download_from_huggingface.py
@@ -1,4 +1,21 @@
+# download_model.py
+import os
 from huggingface_hub import snapshot_download
-snapshot_download(
-    repo_id="<model_name>", revision="<model_revision>", etag_timeout=86400
-)
+
+def main():
+    repo_id = os.getenv("HF_REPO_ID", "bert-base-uncased")
+    revision_id = os.getenv("HF_RVERSION", "main")
+
+    print(f"Start downloading model: {repo_id}")
+    print(f"Target dir: {local_dir}")
+
+    snapshot_download(
+        repo_id=repo_id, 
+        revision=revision_id, 
+        etag_timeout=86400
+    )
+
+    print("Model download finished!")
+
+if __name__ == "__main__":
+    main()
