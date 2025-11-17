d3737efe84440cd33b5d87a554f89e3e8b424346
liwei2151284 <445169331@qq.com>
2025-11-17 16:48:10 +0800

    Update upload_to_AI_Catalog.py

--- upload_to_AI_Catalog.py
+++ upload_to_AI_Catalog.py
@@ -8,10 +8,11 @@ def main():
     revision_id = os.getenv("HF_RVERSION", "main")
 
     base_projects_directory = os.getenv("HF_LOCAL_DIR", f"./models/{repo_id}") 
-    frogml.huggingface.log_model(   
-        repository="kk-huggingface-dev-local",    # The JFrog repository to upload the model to.
-        model_name=repo_id,     # The uploaded model name
-        version=revision_id,     # Optional. The uploaded model version
+    frogml.huggingface.log_model(  
+        source_path = base_projects_directory
+        repository = "kk-huggingface-dev-local",    # The JFrog repository to upload the model to.
+        model_name = repo_id,     # The uploaded model name
+        version = revision_id     # Optional. The uploaded model version
     )
     print("--- Model Logged Successfully ---")
 
